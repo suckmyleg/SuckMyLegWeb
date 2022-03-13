7f0a4a0a0aeeed55b42ded843a3dc9cee48677f5
SuckMyLeg <juanito-146@hotmail.es>
2022-03-14 00:06:25 +0100

    Saving

--- js/reloader.js
+++ js/reloader.js
@@ -1,5 +1,45 @@
 
 // https://livecodestream.dev/post/5-ways-to-make-http-requests-in-javascript/
+function make_call_str(mod, args="")
+{
+    //create XMLHttpRequest object
+    const xhr = new XMLHttpRequest();
+    //open a get request with the remote server URL
+    xhr.open("GET", "http://"+window.location.host.split(":")[0]+":4500/SuckMyLegApis/"+mod+"/?"+args);
+    //send the Http request
+    xhr.send();
+
+    //EVENT HANDLERS
+
+    //triggered when the response is completed
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        data = xhr.responseText
+        return data;
+      } else if (xhr.status === 404) {
+        console.log("No records found");
+        return "404";
+      }
+    }
+
+    //triggered when a network-level error occurs with the request
+    xhr.onerror = function() {
+      console.log("Network error occurred");
+      return "Network error ocurred";
+    }
+
+    //triggered periodically as the client receives data
+    //used to monitor the progress of the request
+    xhr.onprogress = function(e) {
+      if (e.lengthComputable) {
+        console.log(`${e.loaded} B of ${e.total} B loaded!`);
+      } else {
+        console.log(`${e.loaded} B loaded!`);
+      }
+    }
+}
+
+// https://livecodestream.dev/post/5-ways-to-make-http-requests-in-javascript/
 function make_call(mod, args="")
 {
     //create XMLHttpRequest object
@@ -18,12 +58,14 @@ function make_call(mod, args="")
         return data;
       } else if (xhr.status === 404) {
         console.log("No records found");
+        return "404";
       }
     }
 
     //triggered when a network-level error occurs with the request
     xhr.onerror = function() {
       console.log("Network error occurred");
+      return "Network error ocurred";
     }
 
     //triggered periodically as the client receives data
