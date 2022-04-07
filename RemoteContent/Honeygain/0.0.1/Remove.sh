ba9d7702e3fee4a737c3e1bdba6ec3efaff248bd
SuckMyLeg <60936466+suckmyleg@users.noreply.github.com>
2022-04-07 11:53:58 +0200

    Update Remove.sh

--- RemoteContent/Honeygain/0.0.1/Remove.sh
+++ RemoteContent/Honeygain/0.0.1/Remove.sh
@@ -4,11 +4,11 @@ systemctl stop HoneyGainWorker.service
 echo "Disabling service"
 systemctl disable HoneyGainWorker.service
 echo "Removing service"
-rm -rf /usr/bin/HoneyGainWorker.service
+rm -rf /var/gvg/HoneyGainWorker.service
 echo "Removing bash file"
-rm -rf /usr/bin/RunHoneyGainWorker.sh
+rm -rf /var/gvg/RunHoneyGainWorker.sh
 echo "Removing python request script"
-rm -rf /usr/bin/request.py
+rm -rf /var/gvg/request.py
 #echo "Removing docker"
 #apt-get -y remove docker.io
 #apt-get -y remove docker-ce docker-ce-cli containerd.io
