a16f20c0fed49113d4a34dbeb6ed831a82f555aa
SuckMyLeg <60936466+suckmyleg@users.noreply.github.com>
2022-04-07 11:50:18 +0200

    Update Install.sh

--- RemoteContent/Honeygain/0.0.1/Install.sh
+++ RemoteContent/Honeygain/0.0.1/Install.sh
@@ -42,9 +42,7 @@ dir="/var/gvg/"
 
 mkdir $dir
 
-cat >${dir}RunHoneyGainWorker.sh << ENDOFFILE
-$(wget "http://f{$remoteip}:8080/RemoteContent/Honeygain/0.0.1/RunHoneyGainWorker.sh" -q -O -)
-ENDOFFILE
+wget -O ${dir}RunHoneyGainWorker.sh "http://f{remoteip}:8080/RemoteContent/Honeygain/0.0.1/RunHoneyGainWorker.sh" 
 
 ln -s ${dir}HoneyGainWorker.service /etc/systemd/system
 
