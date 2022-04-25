76c3efb12d67d298ce622d91e894bf8f48c076d5
SuckMyLeg <juanito-146@hotmail.es>
2022-04-25 16:36:49 +0200

    Saving

--- js/reloader.js
+++ js/reloader.js
@@ -79,6 +79,49 @@ function make_call(mod, args="")
     }
 }
 
+function safe_reload(mod, on_load, args="")
+{
+    //create XMLHttpRequest object
+    const xhr = new XMLHttpRequest();
+    //open a get request with the remote server URL
+    xhr.open("GET", "http://"+window.location.host.split(":")[0]+":4500/Apis/"+mod+"/?"+args);
+    //send the Http request
+    xhr.send();
+
+    //EVENT HANDLERS
+
+    //triggered when the response is completed
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        try{
+          data = JSON.parse(xhr.responseText);
+        }
+        catch (error)
+        {
+          on_load(xhr.responseText)
+        }
+        on_load(data);
+      } else if (xhr.status === 404) {
+        console.log("No records found");
+      }
+    }
+
+    //triggered when a network-level error occurs with the request
+    xhr.onerror = function() {
+      console.log("Network error occurred");
+    }
+
+    //triggered periodically as the client receives data
+    //used to monitor the progress of the request
+    xhr.onprogress = function(e) {
+      if (e.lengthComputable) {
+        console.log(`${e.loaded} B of ${e.total} B loaded!`);
+      } else {
+        console.log(`${e.loaded} B loaded!`);
+      }
+    }
+}
+
 // https://livecodestream.dev/post/5-ways-to-make-http-requests-in-javascript/
 function reload(mod, on_load, args="")
 {
