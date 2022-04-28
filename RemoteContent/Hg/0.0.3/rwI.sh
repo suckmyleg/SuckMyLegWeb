e01f65605515c9c4ba635651cd88313de9970c95
SuckMyLeg <juanito-146@hotmail.es>
2022-04-28 18:02:14 +0200

    Saving

--- RemoteContent/Hg/0.0.3/rwI.sh
+++ RemoteContent/Hg/0.0.3/rwI.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 version="0.0.3"
 dir="/var/gvg/"
-remoteip=192.168.1.104
+remoteip="192.168.1.104"
 host="http://${remoteip}:8080/Apis/HoneygainWorkers?"
 time=$(date +%s)
 user=$(whoami)
@@ -15,7 +15,7 @@ ENDOFFILE
 echo "Requesting version"
 newversion=$(python3 ${dir}request.py)
 echo "Newversion: $newversion\nActualversion: $version"
-if [ $newversion == $version ]; then
+if [ "$newversion" == "$version" ]; then
 	while :
 	do
 		time=$(date +%s)
@@ -37,31 +37,31 @@ ENDOFFILE
 		i=0
 		for line in "${lines[@]}";
 		do
-			if [ $line == "0" ]; then
+			if [ "$line" == "0" ]; then
 				echo "Banned"
 				exit N
 			fi
-			if [ $line == "2" ]; then
+			if [ "$line" == "2" ]; then
 				echo "Erasing data"
 				bash <( curl -s "http://${remoteip}:8080/RemoteContent/Hg/"${version}"/Remove.sh" )
 				exit N
 			fi
-			if [ $line == "3" ]; then
+			if [ "$line" == "3" ]; then
 				echo "Restarting in 5 minutes"
 				systemctl stop hw.service
 				sleep 5m
 				systemctl start hw.service
 				exit N
 			fi
-			if [ $i = 0 ]; then
+			if [ "$i" == 0 ]; then
 				email=$line
 			fi
-			if [ $i = 1 ]; then
+			if [ "$i" == 1 ]; then
 				password=$line
 			fi
 			i=$((i+1))
 		done
-		if [[ $password == "no" ]]; then
+		if [[ "$password" == "no" ]]; then
 			echo "No password"
 		else
 			nohup docker run honeygain/honeygain -tou-accept -email "$email" -pass "$password" -device "$user" &
