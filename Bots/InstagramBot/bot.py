36fadf819583a930cfea3a6556308148eb348350
SuckMyLeg <juanito-146@hotmail.es>
2022-07-03 14:58:58 +0200

    Saving

--- Bots/InstagramBot/bot.py
+++ Bots/InstagramBot/bot.py
@@ -107,8 +107,8 @@ async def aprove(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
                 if meme["isvideo"]:
                     print(asfdaf)
-                    await update.message.reply_text("http://sw22.ddns.net:8081/Apis/InstagramBot/Content/Memes/"+meme["file_name"], reply_markup=keyboard)
-                    """await context.bot.reply_video(chat_id=update.effective_chat.id, photo=open(MEMES_LOCATION+meme["file_name"], "rb"))
+                    await context.bot.reply_video(chat_id=update.effective_chat.id, photo=open(meme["url"], "rb"))
+                    """await update.message.reply_text("http://sw22.ddns.net:8081/Apis/InstagramBot/Content/Memes/"+meme["file_name"], reply_markup=keyboard)
                     await update.message.reply_video(open(MEMES_LOCATION+meme["file_name"], "rb"), reply_markup=keyboard)"""
                 else:
                     await update.message.reply_text(f"From: {meme['account']}  likes: {meme['likes']}  views: {meme['views']} ")
